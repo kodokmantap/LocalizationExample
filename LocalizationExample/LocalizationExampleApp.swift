aa597e3372af502fae09ca6e0d01f2bb8126bfc3
Yulibar Husni <hyulibar@gmail.com>
2023-07-11 10:39:08 +0700

    Initial Commit

---
+++ LocalizationExample/LocalizationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizationExampleApp.swift
+//  LocalizationExample
+//
+//  Created by Yulibar Husni on 11/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
